9f55c650e8b9246ae201801c4a77bcb7862bb5a7
Roman <Roman@Chicken-2>
2026-09-24 13:23:23 -0700

    Created Card class with fields and constructors

---
+++ src/cardGame/Card.java
@@ -0,0 +1,82 @@
+package cardGame;
+
+import java.util.Objects;
+
+public class Card {
+	
+	private String cardSuit;
+	private String cardName;
+	private int cardValue;
+	private String cardPicture;
+
+	//constructors	
+	public Card(String cardSuit, String cardName, int cardValue, String cardPicture) {
+		super();
+		this.cardSuit = cardSuit;
+		this.cardName = cardName;
+		this.cardValue = cardValue;
+		this.cardPicture = cardPicture;
+	}
+
+	//setters and getters = accessors and mutators
+	public String getCardSuit() {
+		return cardSuit;
+	}
+
+	public void setCardSuit(String cardSuit) {
+		this.cardSuit = cardSuit;
+	}
+
+	public String getCardName() {
+		return cardName;
+	}
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+
+	public int getCardValue() {
+		return cardValue;
+	}
+
+	public void setCardValue(int cardValue) {
+		this.cardValue = cardValue;
+	}
+
+	public String getCardPicture() {
+		return cardPicture;
+	}
+
+	public void setCardPicture(String cardPicture) {
+		this.cardPicture = cardPicture;
+	}
+
+	@Override
+	public String toString() {
+		return "Card [cardSuit=" + cardSuit + ", cardName=" + cardName + ", cardValue=" + cardValue + ", cardPicture="
+				+ cardPicture + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cardName, cardPicture, cardSuit, Integer.valueOf(cardValue));
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		return Objects.equals(cardName, other.cardName) && Objects.equals(cardPicture, other.cardPicture)
+				&& Objects.equals(cardSuit, other.cardSuit) && cardValue == other.cardValue;
+	}
+	
+	
+	
+	
+	
+}
